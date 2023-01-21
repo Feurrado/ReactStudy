2ce23526a09c7d9b1c85629681827a7789f2cb18
Feurrado <thm.profissional@gmail.com>
2023-01-21 20:04:40 -0300

    late nigh commit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, 
   Inter_400Regular, 
   Inter_600SemiBold, 
@@ -7,6 +7,7 @@ import { useFonts,
   from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,22 +24,9 @@ export default function App() {
     }
 
   return (
-    <View style={styles.container}>
-    <Text style={styles.text}>LESDUIT</Text>
+    <>
+    <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
