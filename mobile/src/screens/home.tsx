02a8928bb5e998c691d9aa2004521e46a7345aae
Feurrado <thm.profissional@gmail.com>
2023-01-21 20:07:49 -0300

    fix

--- mobile/src/screens/home.tsx
+++ mobile/src/screens/home.tsx
@@ -13,8 +13,7 @@ export function Home(){
         <Header />
 
         <View className="flex-row mt-6 mb-2">
-            {
-            weekDays.map((weekDay, i) => (
+            {weekDays.map((weekDay, i) => (
                 <Text key={`${weekDay}-${i}`}
                 className="text-zinc-400 text-xl font-bold text-center mx-1"
                 style={{ width: DAY_SIZE }}
